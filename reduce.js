85abca2308aec142a9e3ee8a562c14948d9e15a4
na8esin <na8esin.mpst@gmail.com>
2020-04-24 14:09:11 +0900

    add lodash

--- reduce.js
+++ reduce.js
@@ -19,22 +19,33 @@ obj = [
 const groupBy = (objs, key) => {
   // 初回ループのrvは一番先頭のオブジェクト
   return objs.reduce((rv, obj) => {
+
+    //console.log(rv);
+
     // 更新対象の配列を探す
-    found = rv.find(e => e.userId === obj.userId);
+    if (rv.find(e => e.userId === obj.userId)) {
+      // 見つかった場合更新する
+      // ただ、重複は排除したいね。そんなデータ入るかわからないけど
+      rv.expertiseFields = obj.expertiseFields ?? [];
+      rv.expertiseFields.push(obj.expertiseField);
+    } else {
+      // 見つからなかった場合
+      rv.push({
+        userId: obj.userId,
+        expertiseFields: [{ id: obj.id, name: obj.name }]
+      });
+    }
 
-    // 見つかったやつのexpertiseFieldsに追加する
-    // ただ、重複は排除したいね。そんなデータ入るかわからないけど
-    found.expertiseFields = found.expertiseFields ?? [];
-
-    found.expertiseFields.push();
-
-    (rv[obj["expertiseFields"]] =
-      rv[obj["expertiseFields"]] || []).push(obj.expertiseField);
+    /*
+        (rv[obj["expertiseFields"]] =
+          rv[obj["expertiseFields"]] || []).push(obj.expertiseField);
+    */
     return rv;
-  }, {});
+  }, []); // この第二引数が、rvの初期値か？
 };
 
 console.log(groupBy(obj, 'userId'));
+console.log(groupBy(obj, 'userId')[0]);
 
 /* 最終イメージ
 rv =
