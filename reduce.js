3018db5354c88e7e9d2f54590697f05a25ad5dbb
na8esin <na8esin.mpst@gmail.com>
2020-04-24 14:54:16 +0900

    グルーピング完成

--- reduce.js
+++ reduce.js
@@ -1,3 +1,5 @@
+const util = require('util')
+
 obj = [
   {
     id: 1,
@@ -14,60 +16,69 @@ obj = [
     userId: 100,
     expertiseField: { id: 3, name: '何か' }
   },
+  {
+    id: 4,
+    userId: 90,
+    expertiseField: { id: 2, name: 'ライフプラン作成' }
+  },
+  {
+    id: 5,
+    userId: 90,
+    expertiseField: { id: 2, name: 'ライフプラン作成' }
+  },
+  {
+    id: 6,
+    userId: 90,
+    expertiseField: { id: 1, name: '資産運用A' }
+  },
 ];
 
 const groupBy = (objs, key) => {
   // 初回ループのrvは一番先頭のオブジェクト
   return objs.reduce((rv, obj) => {
-
-    //console.log(rv);
+    console.log('ループ');
+    console.log(util.inspect(rv, { depth: null }));
+    console.log(obj);
 
     // 更新対象の配列を探す
-    if (rv.find(e => e.userId === obj.userId)) {
+    if (found = rv.find(e => e.userId === obj.userId)) {
       // 見つかった場合更新する
-      // ただ、重複は排除したいね。そんなデータ入るかわからないけど
-      rv.expertiseFields = obj.expertiseFields ?? [];
-      rv.expertiseFields.push(obj.expertiseField);
+      if (found.expertiseFields.find(e => e.id === obj.expertiseField.id)) {
+        // 同じデータがすでに登録されている場合はスキップ
+        return rv;
+      }
+      // シャローコピーなのでこれでOK
+      found.expertiseFields.push(obj.expertiseField);
     } else {
       // 見つからなかった場合
       rv.push({
         userId: obj.userId,
-        expertiseFields: [{ id: obj.id, name: obj.name }]
+        expertiseFields: [obj.expertiseField]
       });
     }
-
-    /*
-        (rv[obj["expertiseFields"]] =
-          rv[obj["expertiseFields"]] || []).push(obj.expertiseField);
-    */
+    console.log('リターン直前');
     return rv;
   }, []); // この第二引数が、rvの初期値か？
 };
 
-console.log(groupBy(obj, 'userId'));
-console.log(groupBy(obj, 'userId')[0]);
+console.log(util.inspect(
+  groupBy(obj, 'userId'),
+  { depth: null }));
 
-/* 最終イメージ
-rv =
-  [
-    {
-      userId: 100,
-      expertiseFields:[
-        {
-          id: 1, name: '資産運用A'
-        },
-        {
-          id: 2, name: '年金A'
-        }
-      ]
-    },
-    {
-      userId: 90,
-      expertiseFields:[
-        {
-          id: 2, name: '年金A'
-        }
-      ]
-    }
-  ]
+/* 出力結果
+[
+  {
+    userId: 100,
+    expertiseFields: [
+      { id: 1, name: '資産運用A' },
+      { id: 2, name: '仕事' },
+      { id: 3, name: '何か' }
+    ]
+  },
+  {
+    userId: 90,
+    expertiseFields: [ { id: 2, name: 'ライフプラン作成' }, { id: 1, name: '資産運用A' } ]
+  }
+]
+
 */
