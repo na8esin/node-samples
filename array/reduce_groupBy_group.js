8fd664b680caf3f1003e07f7355083f476fb4d8e
na8esin <na8esin.mpst@gmail.com>
2020-06-12 12:01:47 +0900

    データの順番によってバグってたので修正

--- array/reduce_groupBy_group.js
+++ array/reduce_groupBy_group.js
@@ -3,17 +3,17 @@ const util = require('util')
 // userは複数のグループに所属することができる
 const users = [
   {
-    id: 1,
-    name: 'ユーザ',
+    id: 2,
+    name: 'ユーザ2',
     groups: [
-      {id:24, name:"テストグループ"},
       {id:78, name:"東京グループ"},
     ]
   },
   {
-    id: 2,
-    name: 'ユーザ2',
+    id: 1,
+    name: 'ユーザ',
     groups: [
+      {id:24, name:"テストグループ"},
       {id:78, name:"東京グループ"},
     ]
   },
@@ -24,19 +24,39 @@ const users = [
       {id:24, name:"テストグループ"},
     ]
   },
+  {
+    id: 4,
+    name: 'ユーザ4',
+    groups: [
+      {id:24, name:"テストグループ"},
+      {id:78, name:"東京グループ"},
+    ]
+  },
 ];
 
 const groupBy = (users, key) => {
   // 初回ループのrvは一番先頭のオブジェクト
   return users.reduce((rv, user) => {
-    
-    // まず、groupsのidの配列をつくる？
+
+    // まず、groupsのidの配列をつくる
     const divisonIds = user.groups.map(g => g.id);
-    
-    // 更新対象の配列を探す
-    if (found = rv.find(e => divisonIds.includes(e.divisonId))) {
-      // 見つかった場合追加する。 シャローコピーなのでこれでOK
-      found.users.push(user);
+
+    // 更新対象の配列を探す。
+    // ただ判定するためだけに使ってるので、ここは単純にfindでもいい気がする
+    const founds = rv.filter(e => divisonIds.includes(e.divisonId));
+    if (founds.length > 0) {
+      // すでにあるところに追加して、ない方のやつは新規で追加
+      divisonIds.forEach(divisonId =>{
+        const found = rv.find(e => e.divisonId === divisonId);
+        if (found) {
+          found.users.push(user);
+        } else {
+          rv.push({
+            divisonId: divisonId,
+            users: [user]
+          });
+        }
+      });
     } else {
       // 見つからなかった場合
       user.groups.forEach(g =>{
@@ -55,7 +75,6 @@ console.log(util.inspect(
   { depth: null }));
 
 /* 出力結果
-
 [
   {
     divisonId: 24,
@@ -65,7 +84,12 @@ console.log(util.inspect(
         name: 'ユーザ',
         groups: [ { id: 24, name: 'テストグループ' }, { id: 78, name: '東京グループ' } ]
       },
-      { id: 3, name: 'ユーザ3', groups: [ { id: 24, name: 'テストグループ' } ] }
+      { id: 3, name: 'ユーザ3', groups: [ { id: 24, name: 'テストグループ' } ] },
+      {
+        id: 4,
+        name: 'ユーザ4',
+        groups: [ { id: 24, name: 'テストグループ' }, { id: 78, name: '東京グループ' } ]
+      }
     ]
   },
   {
@@ -76,7 +100,12 @@ console.log(util.inspect(
         name: 'ユーザ',
         groups: [ { id: 24, name: 'テストグループ' }, { id: 78, name: '東京グループ' } ]
       },
-      { id: 2, name: 'ユーザ2', groups: [ { id: 78, name: '東京グループ' } ] }
+      { id: 2, name: 'ユーザ2', groups: [ { id: 78, name: '東京グループ' } ] },
+      {
+        id: 4,
+        name: 'ユーザ4',
+        groups: [ { id: 24, name: 'テストグループ' }, { id: 78, name: '東京グループ' } ]
+      }
     ]
   }
 ]
